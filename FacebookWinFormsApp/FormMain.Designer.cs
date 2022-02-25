98b1cc24ebac0a6b1f6ef0a4bb0ad9ceb8fddc07
matan <matanbare25@gmail.com>
2022-02-25 18:02:40 +0200

    new version

--- FacebookWinFormsApp/FormMain.Designer.cs
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -28,48 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.buttonLogin = new System.Windows.Forms.Button();
-			this.buttonLogout = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// buttonLogin
-			// 
-			this.buttonLogin.Location = new System.Drawing.Point(12, 12);
-			this.buttonLogin.Name = "buttonLogin";
-			this.buttonLogin.Size = new System.Drawing.Size(179, 23);
-			this.buttonLogin.TabIndex = 36;
-			this.buttonLogin.Text = "Login";
-			this.buttonLogin.UseVisualStyleBackColor = true;
-			this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
-			// 
-			// buttonLogout
-			// 
-			this.buttonLogout.Location = new System.Drawing.Point(12, 41);
-			this.buttonLogout.Name = "buttonLogout";
-			this.buttonLogout.Size = new System.Drawing.Size(179, 23);
-			this.buttonLogout.TabIndex = 52;
-			this.buttonLogout.Text = "Logout";
-			this.buttonLogout.UseVisualStyleBackColor = true;
-			this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
-			// 
-			// FormMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(842, 657);
-			this.Controls.Add(this.buttonLogout);
-			this.Controls.Add(this.buttonLogin);
-			this.Name = "FormMain";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Form1";
-			this.ResumeLayout(false);
+            this.pictureBoxCoverPhoto = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProfileImage = new System.Windows.Forms.PictureBox();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelFriendsNumber = new System.Windows.Forms.Label();
+            this.labelFirendsList = new System.Windows.Forms.Label();
+            this.listBoxFrinends = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCoverPhoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfileImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCoverPhoto
+            // 
+            this.pictureBoxCoverPhoto.Location = new System.Drawing.Point(-5, 0);
+            this.pictureBoxCoverPhoto.Name = "pictureBoxCoverPhoto";
+            this.pictureBoxCoverPhoto.Size = new System.Drawing.Size(845, 238);
+            this.pictureBoxCoverPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCoverPhoto.TabIndex = 57;
+            this.pictureBoxCoverPhoto.TabStop = false;
+            // 
+            // pictureBoxProfileImage
+            // 
+            this.pictureBoxProfileImage.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxProfileImage.Location = new System.Drawing.Point(265, 134);
+            this.pictureBoxProfileImage.Name = "pictureBoxProfileImage";
+            this.pictureBoxProfileImage.Size = new System.Drawing.Size(231, 172);
+            this.pictureBoxProfileImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProfileImage.TabIndex = 58;
+            this.pictureBoxProfileImage.TabStop = false;
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(12, 67);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(179, 23);
+            this.buttonLogout.TabIndex = 59;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(12, 28);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(179, 23);
+            this.buttonLogin.TabIndex = 60;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // labelFriendsNumber
+            // 
+            this.labelFriendsNumber.AutoSize = true;
+            this.labelFriendsNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFriendsNumber.Location = new System.Drawing.Point(271, 319);
+            this.labelFriendsNumber.Name = "labelFriendsNumber";
+            this.labelFriendsNumber.Size = new System.Drawing.Size(0, 24);
+            this.labelFriendsNumber.TabIndex = 61;
+            // 
+            // labelFirendsList
+            // 
+            this.labelFirendsList.AutoSize = true;
+            this.labelFirendsList.Location = new System.Drawing.Point(28, 409);
+            this.labelFirendsList.Name = "labelFirendsList";
+            this.labelFirendsList.Size = new System.Drawing.Size(0, 13);
+            this.labelFirendsList.TabIndex = 62;
+            // 
+            // listBoxFrinends
+            // 
+            this.listBoxFrinends.FormattingEnabled = true;
+            this.listBoxFrinends.Location = new System.Drawing.Point(22, 345);
+            this.listBoxFrinends.Name = "listBoxFrinends";
+            this.listBoxFrinends.Size = new System.Drawing.Size(497, 277);
+            this.listBoxFrinends.TabIndex = 63;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 657);
+            this.Controls.Add(this.listBoxFrinends);
+            this.Controls.Add(this.labelFirendsList);
+            this.Controls.Add(this.labelFriendsNumber);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.pictureBoxProfileImage);
+            this.Controls.Add(this.pictureBoxCoverPhoto);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCoverPhoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfileImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
 		#endregion
-
-		private System.Windows.Forms.Button buttonLogin;
-		private System.Windows.Forms.Button buttonLogout;
-	}
+        private System.Windows.Forms.PictureBox pictureBoxCoverPhoto;
+        private System.Windows.Forms.PictureBox pictureBoxProfileImage;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelFriendsNumber;
+        private System.Windows.Forms.Label labelFirendsList;
+        private System.Windows.Forms.ListBox listBoxFrinends;
+    }
 }
 
--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -13,6 +13,11 @@ namespace BasicFacebookFeatures
 {
     public partial class FormMain : Form
     {
+        public User UserResults{ get; set; }
+        public LoginResult Login{ get; set; }
+        public FacebookObjectCollection<Post> UserPosts { get; set; }
+
+
         public FormMain()
         {
             InitializeComponent();
@@ -23,35 +28,90 @@ namespace BasicFacebookFeatures
         {
             Clipboard.SetText("gtaabc92"); /// the current password for Desig Patter
 
-            LoginResult loginResult = FacebookService.Login(
-                    /// (This is Desig Patter's App ID. replace it with your own)
-                    "484536506563845", 
-                    /// requested permissions:
-					"email",
-                    "public_profile",
-                    "user_age_range",
-                    "user_birthday",
-                    "user_events",
-                    "user_friends",
-                    "user_gender",
-                    "user_hometown",
-                    "user_likes",
-                    "user_link",
-                    "user_location",
-                    "user_photos",
-                    "user_posts",
-                    "user_videos"
-                    /// add any relevant permissions
-                    );
+            Login = FacebookService.Login(
+                /// (This is Desig Patter's App ID. replace it with your own)
+                "484536506563845",
+                /// requested permissions:
+                "email",
+                "public_profile",
+                "user_age_range",
+                "user_birthday",
+                "user_events",
+                "user_friends",
+                "user_gender",
+                "user_hometown",
+                "user_likes",
+                "user_link",
+                "user_location",
+                "user_photos",
+                "user_posts",
+                "user_videos",
+                "groups_access_member_info"
+            /// add any relevant permissions
+            );
 
-            buttonLogin.Text = $"Logged in as {loginResult.LoggedInUser.Name}";
 
+            if (!string.IsNullOrEmpty(Login.AccessToken))
+            {
+                UserResults = Login.LoggedInUser;
+                UserPosts = UserResults.Posts;
+
+                buttonLogin.Text = $"Logged in as {Login.LoggedInUser.Name}";
+                Album album = new Album();
+                
+                pictureBoxProfileImage.LoadAsync(UserResults.PictureLargeURL);
+                //pictureBoxCoverPhoto.LoadAsync(UserResults.Albums);
+
+            }
+            // add cover photo
+            foreach (var album in UserResults.Albums)
+            {
+                if (album.Name.Equals("Cover photos"))
+                {
+                    pictureBoxCoverPhoto.LoadAsync(album.PictureAlbumURL);
+                }
+            }
+            // add number of friends
+            if (labelFriendsNumber != null)
+            {
+                labelFriendsNumber.Text = string.Format("Friends: {0}", UserResults.Friends.Count);
+            }
+
+            foreach (var friend in UserResults.Friends)
+            {
+                listBoxFrinends.Items.Add(friend.Name);
+                
+            }
+
+
+            int nuberOfLikes = 0;
+            foreach (var post in UserPosts)
+            {
+                listBoxFrinends.Items.Add("Name:" + post.Name);
+                listBoxFrinends.Items.Add("Description:" + post.Description);
+                listBoxFrinends.Items.Add("Message:" + post.Message);
+                listBoxFrinends.Items.Add("Caption:" + post.Caption);
+                if (post.Like())
+                {
+                    for (int i = 0; i < post.LikedBy.Count; i++)
+                    {
+                        listBoxFrinends.Items.Add("    " + i + 1 + ".Liked by:" + post.LikedBy[i].Name);
+                        nuberOfLikes++;
+                    }
+                }
+                else
+                {
+                    listBoxFrinends.Items.Add("    " + "NO LIKES!!!");
+                }
+
+            }
+            listBoxFrinends.Items.Add("Number Of Likes:" + nuberOfLikes);
         }
 
         private void buttonLogout_Click(object sender, EventArgs e)
         {
-			FacebookService.LogoutWithUI();
-			buttonLogin.Text = "Login";
-		}
-	}
+            FacebookService.LogoutWithUI();
+            buttonLogin.Text = "Login";
+        }
+    }
 }
