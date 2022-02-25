a652878a62f8010ff3862bb4885e494f09371ce8
matan <matanbare25@gmail.com>
2022-02-25 11:45:26 +0200

    Add project files.

---
+++ FacebookWinFormsApp/FormMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace BasicFacebookFeatures
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            FacebookService.s_CollectionLimit = 100;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("gtaabc92"); /// the current password for Desig Patter
+
+            LoginResult loginResult = FacebookService.Login(
+                    /// (This is Desig Patter's App ID. replace it with your own)
+                    "484536506563845", 
+                    /// requested permissions:
+					"email",
+                    "public_profile",
+                    "user_age_range",
+                    "user_birthday",
+                    "user_events",
+                    "user_friends",
+                    "user_gender",
+                    "user_hometown",
+                    "user_likes",
+                    "user_link",
+                    "user_location",
+                    "user_photos",
+                    "user_posts",
+                    "user_videos"
+                    /// add any relevant permissions
+                    );
+
+            buttonLogin.Text = $"Logged in as {loginResult.LoggedInUser.Name}";
+
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+			FacebookService.LogoutWithUI();
+			buttonLogin.Text = "Login";
+		}
+	}
+}
